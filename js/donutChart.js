444f501b801faa95e2610c285b12fda3732e761d
Bryant Rivas <bryantrivas1127@yahoo.com>
2018-08-19 20:20:01 -0400

    Added a few display changes and Cleaned up the javascript functions and onclick features

--- js/donutChart.js
+++ js/donutChart.js
@@ -113,114 +113,93 @@ var myChart = new Chart(atx, {
     }
 });
 
-var menu = document.getElementById('menu');
 
-menu.addEventListener("click", function(){
-var menu = document.getElementById('menu');
-var members = document.getElementById('member');
+
+//Selecting Icons 
+var menu      = document.getElementById('menu');
+var members   = document.getElementById('member');
 var dashboard = document.getElementById('dashboards');
-var visits = document.getElementById('visit');
-var settings = document.getElementById('setting');
-var arrow = document.getElementById('arrow');
-    arrow.className = "highlight";
-    menu.className = "highlight";
-    members.className = "highlight";
-    dashboard.className = "highlight";
-    visits.className = "highlight";
-    settings.className = "highlight";
-var menu = document.getElementById('menu');
-var members = document.getElementById('members');
-var dashboard = document.getElementById('dashboard');
-var visits = document.getElementById('visits');
-var settings = document.getElementById('settings');
-var arrows = document.getElementById('arrows');
-    members.style.display= 'flex';
-    dashboard.style.display= 'flex';
-    visits.style.display= 'flex';
-    settings.style.display= 'flex';
-    menu.style.display= 'flex';
-    arrows.style.display= 'flex';
-});
+var visits    = document.getElementById('visit');
+var settings  = document.getElementById('setting');
+var arrow     = document.getElementById('arrow');
 
+//Open Nav Menu
 
-var arrow = document.getElementById('arrow');
-arrow.addEventListener("click", function myFunction(){
-var menu = document.getElementById('menu');
-var members = document.getElementById('member');
-var dashboard = document.getElementById('dashboards');
-var visits = document.getElementById('visit');
-var settings = document.getElementById('setting');
-var arrow = document.getElementById('arrow');
-    arrow.className = "";
-    menu.className = "";
-    members.className = "";
-    dashboard.className = "";
-    visits.className = "";
-    settings.className = "";
-var menu = document.getElementById('menu');
-var members = document.getElementById('members');
-var dashboard = document.getElementById('dashboard');
-var visits = document.getElementById('visits');
-var settings = document.getElementById('settings');
-var arrows = document.getElementById('arrows');
-    members.style.display= 'none';
-    dashboard.style.display= 'none';
-    visits.style.display= 'none';
-    settings.style.display= 'none';
-    menu.style.display= '';
-    arrows.style.display= 'none';
-});
-var close = document.getElementById('close'); 
-var overlay = document.getElementById('overlay');
-push.addEventListener("click", function myMessage() {
-    var user = document.getElementsByClassName('write')[0].value;
-    var write = document.getElementsByClassName('write')[1].value;
-    var link = document.getElementsByClassName('name');
-    var warningMessage = document.createElement("p");
-    var nodeNoUser = document.createTextNode("No User was selected");
-    var nodeYes = document.createTextNode("Message Sent To " + user);
-    var nodeNoMessage = document.createTextNode("No Message was written")
-    var element = document.getElementById("pop_up");
-    var button = document.createElement("button");
-    var nodeButton = document.createTextNode("Close")
-    if (user === link[0].innerText || user === link[2].innerText || user === link[4].innerText || user === link[6].innerText ){
-       console.log('you got the user right');
-       if (write === ""){
-        console.log('you got no message');
-        warningMessage.appendChild(nodeNoMessage);
-        element.appendChild(warningMessage);
-        button.appendChild(nodeButton);
-        button.setAttribute("id", "close");
-        button.setAttribute("onclick", "closed()");
-        warningMessage.setAttribute("id", "message");
-        element.appendChild(button);
-        overlay.style.display= 'block';
-                        } else {
-                             console.log('you got the message right again');
-                             warningMessage.appendChild(nodeYes);
-                             element.appendChild(warningMessage);
-                             button.appendChild(nodeButton);
-                             element.appendChild(button);
-                             button.setAttribute("id", "close");
-                             button.setAttribute("onclick", "closed()");
-                             warningMessage.setAttribute("id", "message");
-                             overlay.style.display= 'block';
-                             document.getElementById('warning').innerHTML="Success!";
-                             document.getElementById('warning').style.background='#85db80';
+function openMenu(){
+    arrow.className     = "flex highlight";
+    menu.className      = "flex highlight";
+    members.className   = "flex highlight";
+    dashboard.className = "flex highlight";
+    visits.className    = "flex highlight";
+    settings.className  = "flex highlight";
+}
+
+//Closes Nav Menu
+
+function closeMenu(){
+    arrow.className     = "none";
+    menu.className      = "none";
+    members.className   = "none";
+    dashboard.className = "none";
+    visits.className    = "none";
+    settings.className  = "none";
+}
+
+    var close           = document.getElementById('close'); 
+    var overlay         = document.getElementById('overlay');
+function myMessage() {
+    var user            = document.getElementsByClassName('write')[0].value;
+    var write           = document.getElementsByClassName('write')[1].value;
+    var link            = document.getElementsByClassName('name');
+    var warningMessage  = document.createElement("p");
+    var nodeNoUser      = document.createTextNode("No User was selected");
+    var nodeYes         = document.createTextNode("Message Sent To " + user);
+    var nodeNoMessage   = document.createTextNode("No Message was written")
+    var element         = document.getElementById("pop_up");
+    var button          = document.createElement("button");
+    var nodeButton      = document.createTextNode("Close")
+    
+    //if no user is put down
+    if (user !== ""){
+       console.log('you wrote something in user bar good job genius');
+    //if no message is written down
+               if (write !== ""){
+                console.log('you wrote something in the message input good job genius');
+                warningMessage.appendChild(nodeYes);
+                element.appendChild(warningMessage);
+                button.appendChild(nodeButton);
+                button.setAttribute("id", "close");
+                button.setAttribute("onclick", "closed()");
+                warningMessage.setAttribute("id", "message");
+                element.appendChild(button);
+                overlay.style.display= 'block';
+                document.getElementById('warning').innerHTML="Success!";
+                document.getElementById('warning').style.background='#85db80';
+                } else {
+                    console.log('you got the message right again');
+                    warningMessage.appendChild(nodeNoMessage);
+                    element.appendChild(warningMessage);
+                    element.appendChild(button);
+                    button.appendChild(nodeButton);
+                    button.setAttribute("id", "close");
+                    button.setAttribute("onclick", "closed()");
+                    warningMessage.setAttribute("id", "message");
+                    overlay.style.display= 'block';
+                    
                                }
     } else{
-        console.log('you got it the user wrong');
+        console.log("you didnt write anything in the in the user box genius");
         warningMessage.appendChild(nodeNoUser);
         element.appendChild(warningMessage);
+        element.appendChild(button);
         button.appendChild(nodeButton);
         button.setAttribute("id", "close");
         button.setAttribute("onclick", "closed()");
         warningMessage.setAttribute("id", "message");
-        element.appendChild(button);
         overlay.style.display= 'block';
     }          
-});
-  
+}
+  //Closes Modal 
 function closed(){
     overlay.style.display = 'none';
     var take = document.getElementById("close");
